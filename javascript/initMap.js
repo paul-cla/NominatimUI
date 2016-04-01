8e47e0ca0886a41b445131473b5dc636903735bc
Danny Parkinson <danny.parkinson@laterooms.com>
2016-04-01 13:31:14 +0100

    moved all javascript to initmap

--- javascript/initMap.js
+++ javascript/initMap.js
@@ -1,3 +1,6 @@
+function initMap(){
+	google.maps.event.addDomListener(window, "load", initialize);
+}
 function initialize() {
 	map = new google.maps.Map(document.getElementById('map'), {
 		center: {lat: 53.476822, lng: -2.255360}, 
@@ -29,6 +32,56 @@ function initialize() {
 	});
 	drawingManager.setMap(map);
 }
-function initMap(){
-	google.maps.event.addDomListener(window, "load", initialize);
+searchNominatim = function(){
+  results = {};
+  var searchTerm= $('#search-field').val()
+  var country = $("#country-codes").val()
+  $.ajax({
+    dataType: "json",
+    type: "Get",
+    url: "http://nominatim.openstreetmap.org/search.php?format=json&polygon_geojson=1namedetails=1&email=slummock@gmail.com&q=" + searchTerm + "&countrycodes="+country,
+    success: function (data) {
+      $("#results").empty()
+      $.each(data,function(index, item){
+        results[item.osm_id] = item;
+        $("#results").append('<option value = "'+ item.osm_id +'">'+item.display_name+'</option>')
+      })
+      mapDefinition()
+    }
+  });
+}
+
+mapDefinition = function(){
+  var key = $("#results").val();
+  var object = results[key];
+  if(typeof nominatimArea!="undefined")
+  {
+    nominatimArea.setMap(null)
+    map.setCenter(new google.maps.LatLng(53.476822, -2.255360))
+    map.setZoom(6)
+  }
+  if (object.geojson != null)
+    drawGeoJson(object)  
+}
+
+drawGeoJson = function(data){
+
+  map.data.forEach(function(feature) {
+        map.data.remove(feature);
+  });
+
+  var featureCollection = JSON.parse('{"type":"FeatureCollection","features":[{"type":"Feature"}]}');
+
+  featureCollection.features[0].geometry = data.geojson;
+
+  map.data.addGeoJson(featureCollection);
+  map.data.setStyle({
+    fillColor: 'green'
+  });
+
+  var boundingSw = new google.maps.LatLng(data.boundingbox[0], data.boundingbox[2]);
+  var boundingNe = new google.maps.LatLng(data.boundingbox[1], data.boundingbox[3]);
+
+  var boundingBox = new google.maps.LatLngBounds(boundingSw, boundingNe)
+  map.fitBounds(boundingBox)
 }
